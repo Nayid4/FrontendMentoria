c9025062c3fc1be02804ee616928ecc45861e6ba
nayid2004@gmail.com <jmesquivel@unicesar.edu.co>
2025-05-22 19:48:07 -0500

    Agregacion de funcionalidad de reuniones

--- src/app/pages/dashboard/manageActivity/meeting/meeting.component.ts
+++ src/app/pages/dashboard/manageActivity/meeting/meeting.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { TabViewModule } from 'primeng/tabview';
 import { ButtonModule } from 'primeng/button';
-
+import { DialogModule } from 'primeng/dialog';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-meeting',
+  standalone: true,
   imports: [
     TabViewModule,
-    ButtonModule
+    ButtonModule,
+    DialogModule,
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './meeting.component.html',
   styleUrl: './meeting.component.css'
@@ -27,4 +33,46 @@ export class MeetingComponent {
       hora: '14:00 - 15:00'
     }
   ];
+
+  showDialog = false;
+  editMode = false;
+  currentIndex: number | null = null;
+
+  currentReunion = {
+    titulo: '',
+    descripcion: '',
+    fecha: '',
+    hora: ''
+  };
+
+  abrirDialog(reunion?: any, index?: number) {
+    if (reunion) {
+      this.currentReunion = { ...reunion };
+      this.currentIndex = index!;
+      this.editMode = true;
+    } else {
+      this.currentReunion = {
+        titulo: '',
+        descripcion: '',
+        fecha: '',
+        hora: ''
+      };
+      this.editMode = false;
+      this.currentIndex = null;
+    }
+    this.showDialog = true;
+  }
+
+  guardarReunion() {
+    if (this.editMode && this.currentIndex !== null) {
+      this.reuniones[this.currentIndex] = { ...this.currentReunion };
+    } else {
+      this.reuniones.push({ ...this.currentReunion });
+    }
+    this.showDialog = false;
+  }
+
+  eliminarReunion(index: number) {
+    this.reuniones.splice(index, 1);
+  }
 }
