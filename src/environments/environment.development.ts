052c6de70d9a14c0fd134b7544f52568862fd32c
nayid2004@gmail.com <jmesquivel@unicesar.edu.co>
2025-06-10 15:51:13 -0500

    cambio de api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  //apiUrl: 'https://mentoriaurps.azurewebsites.net',
+  //apiUrl: 'https://mentoriaurp.azurewebsites.net',
   apiUrl: 'https://localhost:7204',
   production: false,
 };
